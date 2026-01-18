6feec6e03cb5f6ff309d8e3aa50045066f6feefc
dinooo5252 <firedragon1225@naver.com>
2026-01-18 23:01:09 +0900

    [Feature] Main 씬 UI / 폰트, 배경 에셋 다운로드
    Gamestart, Exit 버튼 기능 추가 완료, Thaleah_PixelFont 에셋, Pixel Skies 배경 에셋 파일 추가

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -69,4 +69,13 @@ public class GameManager : Singleton<GameManager>
 
         SceneManager.LoadScene("LevelSelection");
     }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 }
